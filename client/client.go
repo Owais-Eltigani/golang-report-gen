b0d3a71b6513c1d936b8f7e84d1887ef5855dea5
Owais-Eltigani <ambidextrous.in@gmail.com>
2025-06-18 19:56:12 +0530

    app is working

--- client/client.go
+++ client/client.go
@@ -1,8 +1,36 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	report "github.com/report-gen/reports"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 func main() {
 
 	// making a connection to server
+	conn, err := grpc.NewClient("localhost:5000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	// conn, err :=
+	if err != nil {
+
+		log.Fatal("client: error while conn to server", err)
+		return
+	}
+	//
+	defer conn.Close()
+
+	clinet := report.NewReportServiceClient(conn)
+	res, err := clinet.GenerateReport(context.Background(), &report.ReportRequest{UserId: "5"})
+
+	if err != nil {
+		log.Fatal("client: error in response", err)
+
+		return
+	}
+
+	log.Print("response is: ", res.Status)
+
 }
--- server/server.go
+++ server/server.go
@@ -1,25 +1,50 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	pb "github.com/report-gen/reports"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+// ? implementing the report interface
+type Server struct {
+	pb.UnimplementedReportServiceServer
+}
+
+func (s *Server) GenerateReport(ctx context.Context, reportRequest *pb.ReportRequest) (*pb.ReportResponse, error) {
+	if reportRequest.UserId == "" {
+		log.Fatal("server: error the userid id missing")
+
+		return nil, status.Errorf(codes.NotFound, "userid is missing")
+	}
+
+	log.Printf("report of user: %s, was logged", reportRequest.UserId)
+	return &pb.ReportResponse{ReportId: reportRequest.UserId, Status: "success"}, nil
+}
 
 func main() {
 
 	// create a listener for users requests
-	lis, err := net.Listen("tcp", "5000")
+	lis, err := net.Listen("tcp", ":5000")
 
 	if err != nil {
 		log.Fatal("server: error while creating the listener", err)
 		return
 	}
 
+	// init the grpc server,
 	grpcServer := grpc.NewServer()
+	s := Server{}
+
+	pb.RegisterReportServiceServer(grpcServer, &s)
 
 	//
+	log.Print("server running...")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatal("server: error while init server", err)
 		return
